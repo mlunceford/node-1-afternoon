c3650597833cd351f5315a479b9285865048f609
mlunceford <mark7lunceford@gmail.com>
2019-09-27 16:13:28 -0600

    done

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const getProducts = require('./getProducts');
-const getProduct = require('./getProduct')
+const getProducts = require('./getProducts')
 
 const app = express();
+const port = 3010
 
-const port = 3001;
 
+//! below because i did the callback in one line i didnt have to put {}
+// app.listen(port, () => console.log(`server port running ${port}`))
+// aap.listen(port, () => {
+//         console.log('server listinten prot')
+//     })
+    
+//     ? we made below something we required and exported from a different file so that we could save some space.
+//     app.get('/api/products', (req, res) => {
+//             res.status(200).send(products)
+//         })
+    
 app.get('/api/products', getProducts);
-
-app.get('api/product/:id', getProduct);
-
-app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
-});
+app.get('/api/products/:id', getProducts)
